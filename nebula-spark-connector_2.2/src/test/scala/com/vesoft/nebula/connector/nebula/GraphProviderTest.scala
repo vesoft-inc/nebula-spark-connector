7780baff174fa5606ce8d058509630d85301142b
Nicole00 <anqi.wang@vesoft.com>
2022-05-29 22:16:01 +0800

    spark2.2 reader initial commit

---
+++ nebula-spark-connector_2.2/src/test/scala/com/vesoft/nebula/connector/nebula/GraphProviderTest.scala
@@ -0,0 +1,41 @@
+/* Copyright (c) 2021 vesoft inc. All rights reserved.
+ *
+ * This source code is licensed under Apache 2.0 License.
+ */
+
+package com.vesoft.nebula.connector.nebula
+
+import com.vesoft.nebula.connector.connector.Address
+import com.vesoft.nebula.connector.mock.NebulaGraphMock
+import org.apache.log4j.BasicConfigurator
+import org.scalatest.BeforeAndAfterAll
+import org.scalatest.funsuite.AnyFunSuite
+
+class GraphProviderTest extends AnyFunSuite with BeforeAndAfterAll {
+  BasicConfigurator.configure()
+
+  var graphProvider: GraphProvider = null
+
+  override def beforeAll(): Unit = {
+    val addresses: List[Address] = List(new Address("127.0.0.1", 9669))
+    graphProvider = new GraphProvider(addresses, 3000)
+    val graphMock = new NebulaGraphMock
+    graphMock.mockIntIdGraph()
+    graphMock.mockStringIdGraph()
+    graphMock.close()
+  }
+
+  override def afterAll(): Unit = {
+    graphProvider.close()
+  }
+
+  test("switchSpace") {
+    assert(!graphProvider.switchSpace("root", "nebula", "not_exit_space"))
+    assert(graphProvider.switchSpace("root", "nebula", "test_int"))
+  }
+
+  test("submit") {
+    val result = graphProvider.submit("fetch prop on person 1 yield vertex as v")
+    assert(result.isSucceeded)
+  }
+}
---
+++ nebula-spark-connector_2.2/src/test/scala/com/vesoft/nebula/connector/writer/WriteInsertSuite.scala
@@ -0,0 +1,76 @@
+/* Copyright (c) 2021 vesoft inc. All rights reserved.
+ *
+ * This source code is licensed under Apache 2.0 License.
+ */
+
+package com.vesoft.nebula.connector.writer
+
+import com.vesoft.nebula.client.graph.data.ResultSet
+import com.vesoft.nebula.connector.connector.Address
+import com.vesoft.nebula.connector.mock.{NebulaGraphMock, SparkMock}
+import com.vesoft.nebula.connector.nebula.GraphProvider
+import org.apache.log4j.BasicConfigurator
+import org.scalatest.BeforeAndAfterAll
+import org.scalatest.funsuite.AnyFunSuite
+
+class WriteInsertSuite extends AnyFunSuite with BeforeAndAfterAll {
+  BasicConfigurator.configure()
+
+  override def beforeAll(): Unit = {
+    val graphMock = new NebulaGraphMock
+    graphMock.mockStringIdGraphSchema()
+    graphMock.mockIntIdGraphSchema()
+    graphMock.close()
+  }
+
+  test("write vertex into test_write_string space with insert mode") {
+    SparkMock.writeVertex()
+    val addresses: List[Address] = List(new Address("127.0.0.1", 9669))
+    val graphProvider            = new GraphProvider(addresses, 3000)
+
+    graphProvider.switchSpace("root", "nebula", "test_write_string")
+    val createIndexResult: ResultSet = graphProvider.submit(
+      "use test_write_string; "
+        + "create tag index if not exists person_index on person_connector(col1(20));")
+    Thread.sleep(5000)
+    graphProvider.submit("rebuild tag index person_index;")
+
+    Thread.sleep(5000)
+
+    graphProvider.submit("use test_write_string;")
+    val resultSet: ResultSet =
+      graphProvider.submit("match (v:person_connector) return v;")
+    assert(resultSet.getColumnNames.size() == 1)
+    assert(resultSet.getRows.size() == 13)
+
+    for (i <- 0 until resultSet.getRows.size) {
+      println(resultSet.rowValues(i).toString)
+    }
+  }
+
+  test("write edge into test_write_string space with insert mode") {
+    SparkMock.writeEdge()
+
+    val addresses: List[Address] = List(new Address("127.0.0.1", 9669))
+    val graphProvider            = new GraphProvider(addresses, 3000)
+
+    graphProvider.switchSpace("root", "nebula", "test_write_string")
+    val createIndexResult: ResultSet = graphProvider.submit(
+      "use test_write_string; "
+        + "create edge index if not exists friend_index on friend_connector(col1(20));")
+    Thread.sleep(5000)
+    graphProvider.submit("rebuild edge index friend_index;")
+
+    Thread.sleep(5000)
+
+    graphProvider.submit("use test_write_string;")
+    val resultSet: ResultSet =
+      graphProvider.submit("match (v:person_connector)-[e:friend_connector] -> ()  return e;")
+    assert(resultSet.getColumnNames.size() == 1)
+    assert(resultSet.getRows.size() == 13)
+
+    for (i <- 0 until resultSet.getRows.size) {
+      println(resultSet.rowValues(i).toString)
+    }
+  }
+}

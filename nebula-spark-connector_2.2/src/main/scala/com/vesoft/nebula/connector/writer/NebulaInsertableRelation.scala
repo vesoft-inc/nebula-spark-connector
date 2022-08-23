2252dcd3d7f5b06539cd00d774c68b2b805ace20
Nicole00 <anqi.wang@vesoft.com>
2022-08-23 22:43:59 +0800

    spark connector writer for spark 2.2

---
+++ nebula-spark-connector_2.2/src/main/scala/com/vesoft/nebula/connector/writer/NebulaInsertableRelation.scala
@@ -0,0 +1,13 @@
+/* Copyright (c) 2021 vesoft inc. All rights reserved.
+ *
+ * This source code is licensed under Apache 2.0 License.
+ */
+
+package com.vesoft.nebula.connector.writer
+
+import org.apache.spark.sql.DataFrame
+import org.apache.spark.sql.sources.InsertableRelation
+
+class NebulaInsertableRelation extends InsertableRelation {
+  override def insert(data: DataFrame, overwrite: Boolean): Unit = {}
+}

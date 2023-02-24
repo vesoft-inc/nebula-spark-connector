b6c89b02be0c7954123cc60dfea6682b9c230b15
Anqi <anqi.wang@vesoft.com>
2023-02-24 11:32:17 +0800

    add spark version validate for spark3

--- nebula-spark-connector_3.0/src/main/scala/com/vesoft/nebula/connector/package.scala
+++ nebula-spark-connector_3.0/src/main/scala/com/vesoft/nebula/connector/package.scala
@@ -6,6 +6,7 @@
 package com.vesoft.nebula.connector
 
 import com.vesoft.nebula.connector.ssl.SSLSignType
+import com.vesoft.nebula.connector.utils.SparkValidate
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.{
   DataFrame,
@@ -30,6 +31,7 @@ package object connector {
 
     def nebula(connectionConfig: NebulaConnectionConfig,
                readConfig: ReadNebulaConfig): NebulaDataFrameReader = {
+      SparkValidate.validate("3.0.*", "3.1.*", "3.2.*", "3.3.*")
       this.connectionConfig = connectionConfig
       this.readConfig = readConfig
       this
@@ -179,6 +181,7 @@ package object connector {
       */
     def nebula(connectionConfig: NebulaConnectionConfig,
                writeNebulaConfig: WriteNebulaConfig): NebulaDataFrameWriter = {
+      SparkValidate.validate("3.0.*", "3.1.*", "3.2.*", "3.3.*")
       this.connectionConfig = connectionConfig
       this.writeNebulaConfig = writeNebulaConfig
       this

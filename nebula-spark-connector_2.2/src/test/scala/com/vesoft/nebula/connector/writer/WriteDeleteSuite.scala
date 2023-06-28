617426e9575fb4ea449b566b5167add8f4fbe837
Артём Корсаков <artemkorsakov@mail.ru>
2023-06-28 09:30:09 +0300

    GraphProvider create session only once

--- nebula-spark-connector_2.2/src/test/scala/com/vesoft/nebula/connector/writer/WriteDeleteSuite.scala
+++ nebula-spark-connector_2.2/src/test/scala/com/vesoft/nebula/connector/writer/WriteDeleteSuite.scala
@@ -40,8 +40,8 @@ class WriteDeleteSuite extends AnyFunSuite with BeforeAndAfterAll {
 
   test("write edge into test_write_string space with delete mode") {
     SparkMock.deleteEdge()
-    val addresses: List[Address] = List(new Address("127.0.0.1", "root", "nebula", 9669))
-    val graphProvider = new GraphProvider(addresses, 3000)
+    val addresses: List[Address] = List(new Address("127.0.0.1", 9669))
+    val graphProvider = new GraphProvider(addresses, "root", "nebula", 3000)
 
     graphProvider.switchSpace("test_write_string")
     val resultSet: ResultSet =
